abf2b4a1a9051baf3953bb29d1cb027665407dd5
iamtkk <iamtkk2@gmail.com>
2023-02-05 20:13:24 +0900

    2

--- src/components/RegisterPage/RegisterPage.js
+++ src/components/RegisterPage/RegisterPage.js
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useRef } from "react";
 import { Link } from "react-router-dom";
 import { useForm } from "react-hook-form";
 
 function RegisterPage() {
-  const { register, watch, errors } = useForm({ mode: "onChange" });
+  const { register, watch, errors } = useForm();
+
+  const password = useRef();
+  password.current = watch("password");
 
   console.log(watch("email"));
   return (
@@ -16,15 +19,50 @@ function RegisterPage() {
         <input
           name="email"
           type="email"
-          ref={register({ required: true, maxLength: 10 })}
+          ref={register({ required: true, pattern: /^\S+@\S+$/i })}
         />
-        {errors.email && <p>This field is required</p>}
+        {errors.email && <p>This email field is required</p>}
+
         <label>Name</label>
-        <input name="name" />
+        <input name="name" ref={register({ required: true, maxLength: 10 })} />
+        {errors.name && errors.name.type === "required" && (
+          <p>This name field is required</p>
+        )}
+        {errors.name && errors.name.type === "maxLength" && (
+          <p>Your input exceed maximum length</p>
+        )}
+
         <label>Password</label>
-        <input name="password" type="password" />
+        <input
+          name="password"
+          type="password"
+          ref={register({ required: true, minLength: 6 })}
+        />
+        {errors.password && errors.password.type === "required" && (
+          <p>This password field is required</p>
+        )}
+        {errors.password && errors.password.type === "minLength" && (
+          <p>Password must have at least 6 characters</p>
+        )}
+
         <label>Password Confirm</label>
-        <input name="password_confirm" type="password" />
+        <input
+          name="password_confirm"
+          type="password"
+          ref={register({
+            required: true,
+            validate: (value) => value === password.current,
+          })}
+        />
+        {errors.password_confirm &&
+          errors.password_confirm.type === "required" && (
+            <p>This password confirm field is required</p>
+          )}
+        {errors.password_confirm &&
+          errors.password_confirm.type === "validate" && (
+            <p>The passwords do not match</p>
+          )}
+
         <input type="submit" />
         <Link style={{ color: "gray", textDecoration: "none" }} to="login">
           이미 아이디가 있다면...
